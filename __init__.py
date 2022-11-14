3aef504de441e466c628afa2aba5b91ec5334a92
Sten <62704405+sten-code@users.noreply.github.com>
2022-11-14 21:36:28 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .data import User, Project, Studio, get_user, get_project, get_studio, Message, CommentMessage, FollowUserMessage, FavoriteProjectMessage, LoveProjectMessage, RemixProjectMessage, StudioActivityMessage, InvalidUserException
-from .cloudsession import CloudSession
-from .news import News
-from .session import Session
-from .encoder import encode, encode_list, decode, decode_list, chars
